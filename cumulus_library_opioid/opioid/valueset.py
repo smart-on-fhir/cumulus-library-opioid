9e0b16eb5f7634bc24ee3cf8559a50eda14a7d86
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-06-27 15:16:41 -0700

    * define Opioid overdoses as diagnosis (ICD10 and SNOMED) * define Opioid medications (RXNORM) * define Opioid Lab codes (LOINC)

---
+++ cumulus_library_opioid/opioid/valueset.py
@@ -0,0 +1,170 @@
+import os
+import json
+from typing import List
+from fhirclient.models.coding import Coding
+
+from cumulus_library.helper import load_json
+from cumulus_library.schema.typesystem import Vocab
+import pandas
+
+STUDY_PREFIX = 'opioid'
+
+def get_sheet(sheet_name):
+    """
+    Curated/Supplied by UC Davis
+    :param sheet_name: Excel spreadsheet exported
+    :return:
+    """
+    return pandas.read_excel(get_path('CodeBookOpioidOverDose.xlsx'), sheet_name=sheet_name)
+
+def get_path(filename=None):
+    if filename:
+        return os.path.join(os.path.dirname(__file__), filename)
+    else:
+        return os.path.dirname(__file__)
+
+def escape(sql: str) -> str:
+    """
+    :param sql: SQL potentially containing special chars
+    :return: special chars removed like tic(') and semi(;).
+    """
+    return sql.replace("'", "").replace(";", ".")
+
+def coding2view(view_name: str, concept_list: List[Coding]) -> str:
+    """
+    :param view_name: like define_type
+    :param concept_list: list of concepts to include in definition
+    :return: SQL command
+    """
+    header = f"create or replace view {STUDY_PREFIX}__{view_name} as select * from (values"
+    footer = ") AS t (system, code, display) ;"
+    content = list()
+    for concept in concept_list:
+        safe_display = escape(concept.display)
+        content.append(f"('{concept.system}', '{concept.code}', '{safe_display}')")
+    content = '\n,'.join(content)
+    return header + '\n' + content + '\n' + footer
+
+
+def valueset2coding(valueset_json) -> List[Coding]:
+    """
+    Obtain a list of Coding "concepts" from a ValueSet.
+    This method currently supports only "include" of "concept" defined fields.
+    Not supported: recursive fetching of contained ValueSets, which requires UMLS API Key and Wget, etc.
+
+    examples
+    https://vsac.nlm.nih.gov/valueset/2.16.840.1.113762.1.4.1146.1629/expansion/Latest
+    https://cts.nlm.nih.gov/fhir/res/ValueSet/2.16.840.1.113762.1.4.1146.1629?_format=json
+
+    :param valueset_json: ValueSet file, expecially those provided by NLM/ONC/VSAC
+    :return: list of codeable concepts (system, code, display) to include
+    """
+    filepath = get_path(valueset_json)
+    print(f'\nvalueset:{filepath}')
+
+    valueset = load_json(get_path(valueset_json))
+    parsed = list()
+
+    for include in valueset['compose']['include']:
+        if 'concept' in include.keys():
+            for concept in include['concept']:
+                concept['system'] = include['system']
+                parsed.append(Coding(concept))
+    return parsed
+
+def save(view_name: str, view_sql: str, outfile=None) -> str:
+    """
+    :param view_name: create view as
+    :param view_sql: SQL commands
+    :param outfile: default view_name.sql
+    :return: outfile path
+    """
+    if not outfile:
+        outfile = get_path(f'{view_name}.sql')
+    print(f'\nsave({view_name})')
+    print(f'{outfile}\n')
+    with open(outfile, 'w') as fp:
+        fp.write(view_sql)
+    return outfile
+
+def define_dx_sepsis():
+    """
+    Optional use: SEPSIS is a common and deadly outcome for OUD populations.
+    :return:
+    """
+    codings = valueset2coding('valueset_sepsis_snomed.json')
+    codings += valueset2coding('valueset_sepsis_icd10.json')
+
+    save('define_dx_sepsis', coding2view('define_dx_sepsis', codings))
+
+def define_rx():
+    """
+    Curated/Supplied by UC Davis
+    """
+    df = get_sheet("OpioidMedications")
+
+    col_code = 'RxNORM Concept Unique Identifier (RxCUI)'
+    col_display = 'Drug Name'
+
+    df = df[[col_code, col_display]]
+    codings = list()
+
+    for record in df.to_dict(orient='records'):
+        for code in str(record[col_code]).split(','):
+            as_fhir = {'system': Vocab.RXNORM.value,
+                       'display': record[col_display],
+                       'code': code}
+            codings.append(Coding(as_fhir))
+
+    save('define_rx', coding2view('define_rx', codings))
+
+def define_dx_icd10():
+    """
+    Notice: Epic Bulk-FHIR provides SNOMED not ICD10 !
+    This is for reproducibility
+    """
+    df = get_sheet("DxOpioid-InvolvedOverdoseCodes")
+
+    col_code = 'ICD-10-CM Codes'
+    col_display = 'Description'
+
+    df = df[[col_display, col_code]]
+    codings = list()
+
+    for record in df.to_dict(orient='records'):
+        for code in str(record[col_code]).split(','):
+            as_fhir = {'system': Vocab.ICD10.value,
+                       'display': record[col_display],
+                       'code': code}
+            codings.append(Coding(as_fhir))
+
+    save('define_dx_icd10', coding2view('define_dx_icd10', codings))
+
+def define_lab():
+    """
+    Notice: Epic Bulk-FHIR provides SNOMED not ICD10 !
+    This is for reproducibility
+    """
+    df = get_sheet("Opioid-InvolvedLabCodes")
+
+    col_code = 'LOINC Code'
+    col_display = 'Description'
+
+    df = df[[col_display, col_code]]
+    codings = list()
+
+    for record in df.to_dict(orient='records'):
+        for code in str(record[col_code]).split(','):
+            as_fhir = {'system': Vocab.LOINC.value,
+                       'display': record[col_display],
+                       'code': code}
+            codings.append(Coding(as_fhir))
+
+    save('define_lab', coding2view('define_lab', codings))
+
+
+if __name__ == "__main__":
+    define_rx()
+    define_dx_icd10()
+    define_dx_sepsis()
+    define_lab()
