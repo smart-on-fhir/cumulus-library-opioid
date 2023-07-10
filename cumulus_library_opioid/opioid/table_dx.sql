85d7be28a67e547cd9fe276bb7fb9195dbdb1cd8
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-07-10 05:48:34 -0700

    fixed "Column 'cond_date' cannot be resolved."

--- cumulus_library_opioid/opioid/table_dx.sql
+++ cumulus_library_opioid/opioid/table_dx.sql
@@ -5,6 +5,7 @@ create table opioid__dx as
         dx.display as cond_display,
         c.recorded_month AS cond_month,
         c.recorded_week AS cond_week,
+        c.recordeddate as cond_date,
         date_diff('year', date(p.birthdate), c.recordeddate) AS age_dx_recorded,
         p.gender,
         p.race_display,
