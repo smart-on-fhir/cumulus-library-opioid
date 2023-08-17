5d3ec20bd02a955b5c25352d177f656f11de39a6
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-08-17 10:52:30 -0400

    condition table update

--- cumulus_library_opioid/opioid/count.sql
+++ cumulus_library_opioid/opioid/count.sql
@@ -132,23 +132,23 @@ CREATE TABLE opioid__count_dx_date AS
     ;
 
 -- ###########################################################
-CREATE TABLE opioid__count_dx_sud AS 
-    with powerset as
-    (
-        select
-        count(distinct subject_ref)   as cnt_subject
-        
-        , category_code, cond_display, age_dx_recorded, gender, race_display, ethnicity_display        
-        FROM opioid__dx_sud
-        group by CUBE
-        ( category_code, cond_display, age_dx_recorded, gender, race_display, ethnicity_display )
-    )
-    select
-          cnt_subject as cnt 
-        , category_code, cond_display, age_dx_recorded, gender, race_display, ethnicity_display
-    from powerset 
-    WHERE cnt_subject >= 5 
-    ;
+--CREATE TABLE opioid__count_dx_sud AS 
+--    with powerset as
+--    (
+--        select
+--        count(distinct subject_ref)   as cnt_subject
+--        
+--        , category_code, cond_display, age_dx_recorded, gender, race_display, ethnicity_display        
+--        FROM opioid__dx_sud
+--        group by CUBE
+--        ( category_code, cond_display, age_dx_recorded, gender, race_display, ethnicity_display )
+--    )
+--    select
+--          cnt_subject as cnt 
+--        , category_code, cond_display, age_dx_recorded, gender, race_display, ethnicity_display
+--    from powerset 
+--    WHERE cnt_subject >= 5 
+--    ;
 
 -- ###########################################################
 CREATE TABLE opioid__count_dx_sepsis AS 
