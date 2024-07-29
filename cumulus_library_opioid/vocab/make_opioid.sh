a581803d313070a4f20b2c7a94803bbb0235d029
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-07-29 16:26:30 -0700

    make_opioid.sh now builds iteratively and expands opioid vocab.

---
+++ cumulus_library_opioid/vocab/make_opioid.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+export TSV="/Users/andy/code/medgen-umls/opioid/tsv"
+
+set -e
+export CURATED='opioid'
+source env_table_schema.sh
+
+./create_database.sh
+$mysql_dataset < data/opioid.curated.sql
+./export_tsv.sh curated
+./export_tsv.sh curated_steward
+
+cp $TSV/opioid.curated.tsv data/.
+cp $TSV/opioid.curated_steward.tsv data/.
+
+cd data; rm -f curated.tsv; ln -s opioid.curated.tsv curated.tsv; cd ..
+
+export CURATED='opioid2'
+source db.config
+source env_table_schema.sh
+./make.sh
+./export_tsv.sh curated
+./export_tsv.sh expand_rxcui_str
+cat $TSV/$CURATED.curated.tsv $TSV/$CURATED.expand_rxcui_str.tsv > data/$CURATED.tsv
+cd data; rm -f curated.tsv; ln -s $CURATED.tsv curated.tsv; cd ..
+
+#  export CURATED='opioid3'
+#  source env_table_schema.sh
+#  cd data; rm -f curated.tsv; ln -s opioid2.curated.tsv curated.tsv; cd ..
+#  ./make.sh
+#  ./export_tsv.sh curated
+#  cp /Users/andy/code/medgen-umls/opioid/tsv/opioid3.curated.tsv data/.
+#
+#  export CURATED='opioid4'
+#  source env_table_schema.sh
+#  cd data; rm -f curated.tsv; ln -s opioid3.curated.tsv curated.tsv; cd ..
+#  ./make.sh
+#  ./export_tsv.sh curated
+#  cp /Users/andy/code/medgen-umls/opioid/tsv/opioid4.curated.tsv data/.
+#
+#  export CURATED='opioid5'
+#  source env_table_schema.sh
+#  cd data; rm -f curated.tsv; ln -s opioid4.curated.tsv curated.tsv; cd ..
+#  ./make.sh
+#  ./export_tsv.sh curated
+#  cp /Users/andy/code/medgen-umls/opioid/tsv/opioid5.curated.tsv data/.
+#
+#  export CURATED='opioid6'
+#  source env_table_schema.sh
+#  cd data; rm -f curated.tsv; ln -s opioid5.curated.tsv curated.tsv; cd ..
+#  ./make.sh
+#  ./export_tsv.sh curated
+#  cp /Users/andy/code/medgen-umls/opioid/tsv/opioid6.curated.tsv data/.
