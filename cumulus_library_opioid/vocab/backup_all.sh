76a1aca42e40459953857f97cf02aa516224d564
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-07-30 12:14:27 -0700

    initial import : sh shell scripts to backup/drop mysql ALL databases for this Opioid based study.

---
+++ cumulus_library_opioid/vocab/backup_all.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+source db.config
+source env_table_schema.sh
+
+export CURATED='acep'
+./backup_database.sh
+
+export CURATED='bioportal'
+./backup_database.sh
+
+export CURATED='CancerLinQ'
+./backup_database.sh
+
+export CURATED='ecri'
+./backup_database.sh
+
+export CURATED='impaq'
+./backup_database.sh
+
+export CURATED='lantana'
+./backup_database.sh
+
+export CURATED='math_349'
+./backup_database.sh
+
+export CURATED='medrt'
+./backup_database.sh
+
+export CURATED='mitre'
+./backup_database.sh
+
+export CURATED='CancerLinQ_non'
+./backup_database.sh
+
+export CURATED='mdpartners_non'
+./backup_database.sh
+
+export CURATED='atc_non'
+./backup_database.sh
+
+export CURATED='medrt_non'
+./backup_database.sh
+
+export CURATED='cliniwiz_keywords'
+./backup_database.sh
+
+export CURATED='keywords'
+./backup_database.sh
+
+export CURATED='opioids'
+./backup_database.sh
+
+export CURATED='ucdavis'
+./backup_database.sh
+
+
---
+++ cumulus_library_opioid/vocab/drop_all.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -e
+source db.config
+source env_table_schema.sh
+
+export CURATED='acep'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='atc_non'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='bioportal'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='CancerLinQ'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='cliniwiz'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='cliniwiz_keywords'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='ecri'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='impaq'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='lantana'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='math_349'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='medrt'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='medrt_non'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='mitre'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='CancerLinQ_non'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='mdpartners_non'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+###############################################################################
+# Validation: UCDAVIS Epic EHR real world data of prescriptions
+
+export CURATED='ucdavis'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+###############################################################################
+# Cumulus Opioid Vocabulary
+
+export CURATED='opioid'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='opioid2'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='opioid3'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='opioid4'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='opioid5'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+export CURATED='opioid6'
+mysql -u root -e "drop database if exists ${CURATED}"
+
+
