a57186872f323cb10e53b348043575218f7d45da
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-08-08 11:49:27 -0700

    human_import.sql
    import results from human expert reviews

--- cumulus_library_opioid/vocab/validate/human/human_import.sql
+++ cumulus_library_opioid/vocab/validate/human/human_import.sql
@@ -14,6 +14,9 @@ create  table           human
 
 call create_index('human', 'RXCUI');
 
+-- ##############################################
+call log('human_ucdavis', 'begin');
+
 drop   table if exists  human_ucdavis;
 create table            human_ucdavis like human;
 
@@ -22,6 +25,47 @@ local   infile     'human_ucdavis.tsv'
 into    table       human_ucdavis;
 show warnings;
 
+delete from human_ucdavis where RXCUI = 'RXCUI';
+
+-- ##############################################
+call log('human_keywords', 'begin');
+
+drop   table if exists  human_keywords;
+create table            human_keywords like human;
+
+load    data
+local   infile     'human_keywords.tsv'
+into    table       human_keywords;
+show warnings;
+
+delete from human_keywords where RXCUI = 'RXCUI';
+
+
+-- ##############################################
+call log('human_atc_non', 'begin');
+
+drop   table if exists  human_atc_non;
+create table            human_atc_non like human;
+
+load    data
+local   infile     'human_atc_non.tsv'
+into    table       human_atc_non;
+show warnings;
+
+delete from human_atc_non where RXCUI = 'RXCUI';
+
+-- ##############################################
+call log('human_medrt_non', 'begin');
+
+drop   table if exists  human_medrt_non;
+create table            human_medrt_non like human;
+
+load    data
+local   infile     'human_medrt_non.tsv'
+into    table       human_medrt_non;
+show warnings;
+
+delete from human_medrt_non where RXCUI = 'RXCUI';
 
 
 
